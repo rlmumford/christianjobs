49fcf88be29ae90cc4fa6d1b1842dfb70008991e
Rob Mumford <rlmumford@googlemail.com>
2018-08-28 23:45:18 +0100

    by rlmumford: Post Job redirect to register page.

--- web/modules/custom/job_board/src/Controller/JobBoardController.php
+++ web/modules/custom/job_board/src/Controller/JobBoardController.php
@@ -3,6 +3,8 @@
 namespace Drupal\job_board\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class JobBoardController extends ControllerBase {
 
@@ -10,9 +12,16 @@ class JobBoardController extends ControllerBase {
    * Page to post a new job.
    */
   public function postJob() {
+    $current_user = \Drupal::currentUser();
+    if ($current_user->isAnonymous() && !$current_user->hasPermission('post job board jobs')) {
+      return new RedirectResponse(
+        Url::fromRoute('user.register', [], ['query' => ['destination' => 'jobs/post']])->setAbsolute()->toString()
+      );
+    }
+
     /** @var \Drupal\job_board\JobBoardJobRole $job */
     $job = $this->entityTypeManager()->getStorage('job_role')->create([]);
-    $job->setOwnerId(\Drupal::currentUser()->id());
+    $job->setOwnerId($current_user->id());
     return $this->entityFormBuilder()->getForm($job, 'post');
   }
 }
