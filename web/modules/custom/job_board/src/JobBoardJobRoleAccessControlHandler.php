b485749a2bafe9623db60f70a5d596cc45585a01
rlmumford <rlmumford@googlemail.com>
2018-10-31 11:42:21 +0000

    by rlmumford: Added Access Control to job edit page.

--- web/modules/custom/job_board/src/JobBoardJobRoleAccessControlHandler.php
+++ web/modules/custom/job_board/src/JobBoardJobRoleAccessControlHandler.php
@@ -28,10 +28,12 @@ class JobBoardJobRoleAccessControlHandler extends JobRoleAccessControlHandler {
     }
     if ($operation == 'update') {
       $result = AccessResult::allowedIfHasPermission($account, 'update any job_role');
-      $result->orIf(
+      $result = $result->orIf(
         AccessResult::allowedIfHasPermission($account, 'update own job_role')
           ->andIf(AccessResult::allowedIf($entity->owner->target_id == $account->id()))
       );
+      $result->cachePerUser();
+      $result->addCacheableDependency($entity);
       return $result;
     }
 
