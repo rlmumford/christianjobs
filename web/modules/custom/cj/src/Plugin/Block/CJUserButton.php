bcfed70936ce0958aac9e0db617abacacc810c55
Rob Mumford <rlmumford@googlemail.com>
2018-11-08 11:47:49 +0000

    by rlmumford: Fix faulty html in user link.

--- web/modules/custom/cj/src/Plugin/Block/CJUserButton.php
+++ web/modules/custom/cj/src/Plugin/Block/CJUserButton.php
@@ -20,13 +20,13 @@ class CJUserButton extends BlockBase {
   public function build() {
     $class = \Drupal::currentUser()->isAuthenticated() ? 'user-authenticated' : 'user-anonymous';
     $title = \Drupal::currentUser()->isAuthenticated() ? 'Your Dashboard' : 'Log-in';
-    $markup = '<a title="'.$title.'" href="/user" rel="no-follow"><i class="material-icons navbar-icon '.$class.'">account_circle</i></a>';
+    $markup = '<a title="'.$title.'" href="/user" rel="nofollow"><i class="material-icons navbar-icon '.$class.'">account_circle</i></a>';
 
     if ($cart = \Drupal::service('commerce_cart.cart_provider')->getCart('default')) {
       /** @var \Drupal\commerce_order\Entity\OrderInterface $cart */
       $item_count = count($cart->getItems());
       $items_class = $item_count > 0 ? 'has-items has-'.$item_count.'-items' : 'no-items';
-      $markup = '<a title="Your Cart" href="/cart" rel="no-follow"><i item-count="'.$item_count.'" class="material-icons navbar-icon cart-icon '.$items_class.'">shopping_basket</i></a>' . $markup;
+      $markup = '<a title="Your Cart" href="/cart" rel="nofollow"><i item-count="'.$item_count.'" class="material-icons navbar-icon cart-icon '.$items_class.'">shopping_basket</i></a>' . $markup;
     }
 
     return [
