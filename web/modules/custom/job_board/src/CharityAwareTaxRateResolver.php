0b520c86fe6fdaab023faed65d0ca2dc6474f1e5
Rob Mumford <rlmumford@googlemail.com>
2019-02-25 15:47:15 +0000

    by rlmumford: Update to tax system to make it awa

--- web/modules/custom/job_board/src/CharityAwareTaxRateResolver.php
+++ web/modules/custom/job_board/src/CharityAwareTaxRateResolver.php
@@ -11,6 +11,9 @@ namespace Drupal\job_board;
 use Drupal\commerce_order\Entity\OrderItemInterface;
 use Drupal\commerce_tax\Resolver\DefaultTaxRateResolver;
 use Drupal\commerce_tax\TaxZone;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\job_board\Entity\JobExtension;
+use Drupal\job_role\Entity\JobRole;
 use Drupal\profile\Entity\ProfileInterface;
 
 /**
@@ -24,20 +27,58 @@ use Drupal\profile\Entity\ProfileInterface;
 class CharityAwareTaxRateResolver extends DefaultTaxRateResolver {
 
   /**
+   * @var \Drupal\profile\ProfileStorageInterface
+   */
+  protected $profileStorage = NULL;
+
+  /**
+   * CharityAwareTaxRateResolver constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->profileStorage = $entity_type_manager->getStorage('profile');
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function resolve(TaxZone $zone, OrderItemInterface $order_item, ProfileInterface $customer_profile) {
+    // Check that the order item entity is a job role.
+    $purchased_entity = $order_item->getPurchasedEntity();
+    if (!(($purchased_entity instanceof JobRole) || ($purchased_entity instanceof JobExtension))) {
+      return FALSE;
+    }
+
+    // Get the employer profile to check the charity profile.
+    $user = $customer_profile->getOwner();
+    $employer_profile = $this->profileStorage->loadDefaultByUser($user, 'employer');
+    if (!$employer_profile) {
+      return FALSE;
+    }
+
+    if (!$employer_profile->employer_is_charity->value) {
+      return FALSE;
+    }
+
+    if ($zone->getId() != "gb") {
+      return FALSE;
+    }
+
+    // Get the available rates for this zone.
     $rates = $zone->getRates();
 
-    dpm($rates, "Rates");
-    // Take the default rate, or fallback to the first rate.
-    $resolved_rate = reset($rates);
+    $resolved_rate = FALSE;
     foreach ($rates as $rate) {
-      if ($rate->isDefault()) {
+      if ($rate->getId() == "zero") {
         $resolved_rate = $rate;
         break;
       }
     }
+
     return $resolved_rate;
   }
 }
