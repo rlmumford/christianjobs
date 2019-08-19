30e83fb4ab3d6efb94efb45ecbc914578f1a33fb
Rob Mumford <rlmumford@googlemail.com>
2019-08-19 19:07:37 +0100

    by rlmumford: Candidate registration

--- web/themes/custom/cj_material/js/custom.js
+++ web/themes/custom/cj_material/js/custom.js
@@ -1,7 +1,28 @@
-(function ($) {
-  $(document).ready(function () {
+/**
+ * @file
+ * Select2 integration.
+ */
+(function ($, drupalSettings, Drupal) {
+  'use strict';
 
-    // put your code here
+  Drupal.behaviors.cj_material_radios = {
+    attach: function (context) {
+      $('input.account-type-radios').parent().addClass('btn');
+      $('input.form-radio').each(function() {
+        if ($(this).is(':checked')) {
+          $(this).parent().addClass('form-type-radio-checked');
+        }
+      });
+      $('input.form-radio').change(function () {
+        if ($(this).is(':checked')) {
+          $('input.form-radio[name="'+$(this).attr('name')+'"]').parent().removeClass('form-type-radio-checked');
+          $(this).parent().addClass('form-type-radio-checked');
+        }
+        else {
+          $(this).parent().removeClass('form-type-radio-checked');
+        }
+      });
+    }
+  };
 
-  }); 
-}(jQuery));
+})(jQuery, drupalSettings, Drupal);
