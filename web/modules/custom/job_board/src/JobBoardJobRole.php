92d82dd3ad34fc7885beb5f1fb02945ad6599509
rlmumford <rlmumford@googlemail.com>
2020-02-10 08:55:02 +0000

    by rlmumford: Hotfixes.

--- web/modules/custom/job_board/src/JobBoardJobRole.php
+++ web/modules/custom/job_board/src/JobBoardJobRole.php
@@ -156,7 +156,7 @@ class JobBoardJobRole extends JobRole implements PurchasableEntityInterface {
    */
   public function getPrice(Context $context = NULL) {
     if ($this->rpo->value) {
-      $price = new Price('1495.00', 'GBP');
+      $price = new Price('2995.00', 'GBP');
     }
     else if ($this->initial_duration->value == 'P60D') {
       $price = new Price('100.00', 'GBP');
