7ec7bba876fe34a019aa728713ece07998c818dd
rlmumford <rlmumford@googlemail.com>
2020-02-10 08:55:02 +0000

    by rlmumford: Hotfixes.

--- web/modules/custom/cj/src/Plugin/Block/CJFrontPagePartners.php
+++ web/modules/custom/cj/src/Plugin/Block/CJFrontPagePartners.php
@@ -102,19 +102,19 @@ class CJFrontPagePartners extends BlockBase implements ContainerFactoryPluginInt
           'TGBC-logo.png',
           Url::fromUri('https://www.thegoodbook.co.uk/')
         ),
-        'rock' => $this->buildPartnerCard(
-          'Rock UK',
-          'Outdoor Adventure for All',
-          'Four nationwide centres offer tailor-made programmes of challenging activities to encourage resilience, confidence and independence.',
-          'rockuklogo.svg',
-          Url::fromUri('https://rockuk.org')
+        'trustedhr' => $this->buildPartnerCard(
+          'Trusted HR',
+          'HR support tailored to your values and needs',
+          'We take time to get to know every church and/or a charity to fully match our service to them. We are passionate and understanding the aims, mission, values and needs.',
+          'trustedhrlogo.png',
+          Url::fromUri('https://trustedhr.co.uk/cj')
         ),
-        'endis' => $this->buildPartnerCard(
-          'Church Insight',
-          'Website, Fundraising and Online Marketing',
-          'Church Insight are experts in designing and building easy-to-use websites for Christian organisations.',
-          'ChurchInsight-logo-750.png',
-          Url::fromUri('https://churchinsight.co.uk/')
+        'css' => $this->buildPartnerCard(
+          'Christian Safeguarding Services',
+          'Safeguarding training to churches and faith-based organisations',
+          'Christian Safeguarding Service support churches and faith-based organisations with policy development, bespoke training and consultancy services. We have a support and advice line and an answerphone service.',
+          'css-logo.png',
+          Url::fromUri('https://christiansafeguardingservices.phasic-ltd.co.uk/')
         ),
         'cpo' => $this->buildPartnerCard(
           'CPO',
