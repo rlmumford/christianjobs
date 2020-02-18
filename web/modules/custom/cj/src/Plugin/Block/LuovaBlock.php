569e6a9684c8fc8254611cf91ec5cb103c58e692
BenPoole <ben.poole@christianjobs.co.uk>
2020-02-18 12:25:18 +0000

    Further requested changes and restructuring to Luova responsive banner

--- web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
+++ web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
@@ -22,10 +22,14 @@ class LuovaBlock extends BlockBase {
   public function build() {
     $content = [];
     $content['#markup'] = '<div class="row ml-24">
-<div class="col col-md-offset-3 col-md-9 offset-4">
+<div class="col col-xs-12 col-md-2 col-md-offset-1 first-md luova-logo-wrapper">
+<img src="'.drupal_get_path('theme', 'cj_material').'/assets/LuovaLogo.png" class="luova-banner-logo mt-24" />
+<img src="'.drupal_get_path('theme', 'cj_material').'/assets/VCISLogo.png" class="luova-banner-logo mt-24" />
+</div>
+<div class="col col-xs-12 col-md-9">
 <h2>Are you passionate about impacting the next generation through education?</h2>
 
-<p><span>On behalf of Luova Education Group and Victory Christian International School, Christian Jobs are actively recruiting teachers to join an exciting new school program in Busan, South Korea.</span></p>
+<p><span>In partnership with <span class="luova-education-group">Luova Education Group</span> and <span class="victory-christian-international-school">Victory Christian International School</span> Christian Jobs are actively recruiting teachers to join an exciting new Christian school program in Busan, South Korea.</span></p>
 <a class="btn" href="https://www.christianjobs.co.uk/jobs/christian-jobs-ltd/1333">Find out more</a></div>
 </div>';
 
