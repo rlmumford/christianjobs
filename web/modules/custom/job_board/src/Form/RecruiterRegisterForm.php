e02a39bbcb6e286c7727953504b15b0c533f1ea0
Rob Mumford <rlmumford@googlemail.com>
2020-06-01 18:22:40 +0100

    by rlmumford: Recruiter pages, hq field and jobs view.

--- web/modules/custom/job_board/src/Form/RecruiterRegisterForm.php
+++ web/modules/custom/job_board/src/Form/RecruiterRegisterForm.php
@@ -2,8 +2,18 @@
 
 namespace Drupal\job_board\Form;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\user\RegisterForm;
 
 class RecruiterRegisterForm extends RegisterForm {
+
+  public function save(array $form, FormStateInterface $form_state) {
+    parent::save($form, $form_state);
+
+    // If the user is logged in as part of registration, then
+    if (\Drupal::currentUser()->isAuthenticated()) {
+      $form_state->setRedirect('job_board.recruiter.register.organization');
+    }
+  }
 
 }
---
+++ web/modules/custom/job_board/src/Form/RecruiterRegisterOrganizationForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rob
+ * Date: 01/06/2020
+ * Time: 13:40
+ */
+
+namespace Drupal\job_board\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+
+class RecruiterRegisterOrganizationForm extends ContentEntityForm {
+
+}
