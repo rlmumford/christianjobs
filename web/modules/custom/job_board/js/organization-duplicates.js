133dc01b478bf629a0789661f69790033a4fbdc6
Rob Mumford <rlmumford@googlemail.com>
2020-06-04 12:08:32 +0100

    by rlmumford: Get popup properly.

--- web/modules/custom/job_board/js/organization-duplicates.js
+++ web/modules/custom/job_board/js/organization-duplicates.js
@@ -1,7 +1,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.jobBoardOrganizationDuplicates = {
     attach: function(context) {
-      // @todo: Register on click.
+      $(".field--name-headquarters select.country.form-select").once("register-dups-trigger").each(function () {
+        $(this).change(function () {
+          $(this.form).find("input[type=\"submit\"][name=\"find_dups\"]").mousedown();
+        })
+      });
     }
   };
 
