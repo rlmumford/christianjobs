5787f921fb71089ea6c95f3e957295531b7303c2
BenPoole88 <ben.poole@christianjobs.co.uk>
2021-01-06 12:48:05 +0000

    by Ben Poole: further styling to LCM banner

--- web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
+++ web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
@@ -19,15 +19,15 @@ class LuovaBlock extends BlockBase {
    */
   public function build() {
     $content = [];
-    $content['#markup'] = '<div class="row ml-24">
-<div class="col col-xs-12 col-md-2 first-md luova-logo-wrapper">
-<img src="/'.drupal_get_path('theme', 'cj_material').'/assets/lcmlogo.png" class="luova-banner-logo mt-24" />
+    $content['#markup'] = '<div class="row lcm-banner-container">
+<div class="col col-xs-12 col-md-3 first-md lcm-logo-wrapper">
+<img src="/'.drupal_get_path('theme', 'cj_material').'/assets/lcmlogo.png" class="lcm-banner-logo" />
 </div>
-<div class="col col-xs-12 col-md-9">
+<div class="col col-xs-12 col-md-9 lcm-banner-text">
 <h2>Are you ready for a new fundraising opportunity?</h2>
 
 <p><span>Christian Jobs are currently recruiting for London City Mission, a London-focused Christian organisation equipping missionaries to reach the streets of London who are looking to expand their fundraising team through a number of opportunities. If you are a fundraiser, or know someone who may be interested in one of these new and exciting opportunities, contact our recruitment team to find out more!</span></p>
-<a class="btn" href="https://www.christianjobs.co.uk/jobs?label=&organisation=London+City+Mission">Find out more</a></div>
+<a class="btn" href="https://www.christianjobs.co.uk/employer/54">Find out more</a></div>
 </div>';
     return $content;
   }
