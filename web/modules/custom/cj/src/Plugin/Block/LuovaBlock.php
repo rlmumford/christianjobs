7ec4b7e5110add314be68bc87b0bcfe8f3fb74cf
BenPoole88 <58986635+BenPoole88@users.noreply.github.com>
2021-10-14 13:31:47 +0100

    Updated WW banner text (#40)
    * by Ben Poole: updated to new ddev site
    
    * by Ben Poole: updated banner header text per JE request

--- web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
+++ web/modules/custom/cj/src/Plugin/Block/LuovaBlock.php
@@ -24,7 +24,7 @@ class LuovaBlock extends BlockBase {
 <img src="/'.drupal_get_path('theme', 'cj_material').'/assets/wwlogo.png" class="ww-banner-logo" />
 </div>
 <div class="col col-xs-12 col-md-9 ww-banner-text">
-<h2>Would you like to see your work more as worship as you seek to transform nations?</h2>
+<h2>Christian Graduate Internship in Workplace Ministry</h2>
 
 <p><span class="ww-highlight">Worship.Works</span> trains followers of Jesus to see their work as worship, with endless possibilities. Their vision is to equip thousands of people to minister through their work by offering teaching, speaking and publishing online resources. They are looking for people to join their <span class="ww-highlight">Christian Graduate Internship in Workplace Ministry</span>. If you or someone you know may be interested in this opportunity then contact our recruitment team today!</span></span></p>
 <a class="btn" href="https://www.christianjobs.co.uk/jobs/worshipworks/2783">Find out more</a></div>
